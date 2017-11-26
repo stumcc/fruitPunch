fdab2fc6d51d56ac389f599c10002202d471af1e
Stuart McColl <stuart.j.mccoll@accenture.com>
2017-11-26 10:20:09 +0000

    first commit

---
+++ src/main/java/fruitPunch/App.java
@@ -0,0 +1,13 @@
+package fruitPunch;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello Stuarty!" );
+    }
+}
